70a5c3e142fec7d7684a50b847d1f4d0cd87a239
Nebojsa-Petrovic92 <nebojsa@madeonline.com>
2023-06-29 16:02:09 +0200

    padding2

--- template-parts/content-why.php
+++ template-parts/content-why.php
@@ -19,7 +19,7 @@
             <div class="row">
                 <div class="col-md-3 col-sm-6 col-xs-12">
                     <p class="why-bold">
-                        <span>Testing</span>
+                        <span>Testing span</span>
                     </p>
                 </div>
             </div>
