6ff01e52f37c2fef048957aa551e0bb4485a6e99
Nebojsa-Petrovic92 <nebojsa@madeonline.com>
2023-06-30 17:29:56 +0200

    about start

--- page-templates/about-template.php
+++ page-templates/about-template.php
@@ -10,7 +10,58 @@ if ( ! defined( 'ABSPATH' ) ) {
 get_header();
 ?>
 
-	
-	
+<header class="about-header">
+    <div class="container">
+        <div class="row">
+            <div class="col-md-12">
+                <div class="header-caption">
+                    <h1><?php the_field('title_about'); ?></h1>
+                    <!-- <div class="wave"></div> -->
+
+                </div>
+                <!-- /.header-caption -->
+            </div>
+            <!-- /.col-md-12 -->
+        </div>
+        <!-- /.row -->
+    </div>
+    <!-- /.container -->
+</header>
+
+<div id="about-main">
+    <div class="container">
+        
+        <div class="about-items">
+
+		<?php if( have_rows('content_blocks_about') ): ?>
+			<?php while( have_rows('content_blocks_about') ): the_row(); ?>
+
+				<section class="about-item">
+					<div class="row">						
+						<div class="col-lg-12">
+							<div class="about-content">
+								<?php the_sub_field('content_block'); ?>
+							</div>
+							<!-- /.about-content -->
+						</div>
+						<!-- /.col-lg-7 -->
+					</div>
+					<!-- /.row -->
+				</section>
+				<!-- /.about-item -->
+
+								
+
+			<?php endwhile; ?>
+		<?php endif; ?>
+
+        </div>
+        <!-- /.about-items -->
+
+        
+    </div>
+    <!-- /.container -->
+</div>
+<!-- /#about-main -->
 
 <?php get_footer(); ?>
